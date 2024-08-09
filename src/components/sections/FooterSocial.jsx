9cb1b5619ff184a6cb245af7ffd8d5a10a8925f3
Gabriel <gabrielsouza65115@gmail.com>
2024-08-09 15:03:09 -0300

    feat: Adiciona o botão para o Twiter no footer

--- src/components/sections/FooterSocial.jsx
+++ src/components/sections/FooterSocial.jsx
@@ -174,13 +174,13 @@ function FooterSocial() {
                   }
                 />
               </a>
-              {/* <a
-                href={links.socialMedia.facebook}
+              <a
+                href={links.socialMedia.twitter}
                 target="_blank"
-                aria-label="Link para o Facebook"
+                aria-label="Link para o Twitter"
               >
                 <IconButton
-                  ariaLabel="Botão para o Facebook"
+                  ariaLabel="Botão para o Twitter"
                   icon={
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -192,13 +192,13 @@ function FooterSocial() {
                       stroke-width="2"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      class="lucide lucide-facebook"
+                      class="lucide lucide-twitter"
                     >
-                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                     </svg>
                   }
                 />
-              </a> */}
+              </a>
               {/* <a href={links.socialMedia.linkedin} target="_blank">
                   <IconButton
                     icon={
