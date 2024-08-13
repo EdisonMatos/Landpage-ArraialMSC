3a95447887fb2d704d55289d3e882674c98aa46d
Gabriel <gabrielsouza65115@gmail.com>
2024-08-13 09:36:53 -0300

    fix: Insere animação de entrada no Section Header da Sessão Features

--- src/components/sections/FeaturesWithIcons.jsx
+++ src/components/sections/FeaturesWithIcons.jsx
@@ -13,21 +13,21 @@ import Icon4 from "../../assets/imgs/icons/icon4.png";
 export default function FeaturesWithIcons() {
   return (
     <SectionArea id="service" className="" paddingbot={false}>
-      <SectionHeader
-        className="text-center"
-        miniTitle={content.texts.features.miniTag}
-        sectionHeaderTitle={content.texts.features.title}
-        sectionHeaderSubtitle={content.texts.features.subtitle}
-        color="dark"
-      />
+      <MotionDivDownToUp className="w-full flex justify-center">
+        <SectionHeader
+          className="text-center"
+          miniTitle={content.texts.features.miniTag}
+          sectionHeaderTitle={content.texts.features.title}
+          sectionHeaderSubtitle={content.texts.features.subtitle}
+          color="dark"
+        />
+      </MotionDivDownToUp>
       <SectionWrapper>
         <div className="flex flex-col items-center justify-between w-full tablet1:flex-row">
           <div className="col1">
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={
-                  <img src={Icon1} alt=""/>
-                }
+                icon={<img src={Icon1} alt="" />}
                 title={content.texts.features.card1.title}
                 paragraph={content.texts.features.card1.subtitle}
                 className="tablet1:mb-[46px]"
@@ -35,9 +35,7 @@ export default function FeaturesWithIcons() {
             </MotionDivDownToUp>
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={
-                  <img src={Icon2} alt=""/>
-                }
+                icon={<img src={Icon2} alt="" />}
                 title={content.texts.features.card2.title}
                 paragraph={content.texts.features.card2.subtitle}
               />
@@ -54,9 +52,7 @@ export default function FeaturesWithIcons() {
           <div className="col3 ">
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={
-                  <img src={Icon3} alt=""/>
-                }
+                icon={<img src={Icon3} alt="" />}
                 title={content.texts.features.card3.title}
                 paragraph={content.texts.features.card3.subtitle}
                 className="tablet1:mb-[46px]"
@@ -64,9 +60,7 @@ export default function FeaturesWithIcons() {
             </MotionDivDownToUp>
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={
-                  <img src={Icon4} alt=""/>
-                }
+                icon={<img src={Icon4} alt="" />}
                 title={content.texts.features.card4.title}
                 paragraph={content.texts.features.card4.subtitle}
               />
