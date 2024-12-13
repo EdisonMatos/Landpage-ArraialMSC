94a8bf61c00856b1e8c04cc72f4f07dd2c7c5bd8
Gabriel <gabrielsouza65115@gmail.com>
2024-12-13 17:49:31 -0300

    fix: Sobe versão de modal para celular pronta

--- src/components/sectionElements/Reservation.jsx
+++ src/components/sectionElements/Reservation.jsx
@@ -8,24 +8,131 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Dropdown } from "primereact/dropdown";
 // import Button from "@mui/material/Button";
 import Button from "../../components/interactives/Button";
+import { Dialog } from "primereact/dialog";
 
 export default function Reservation({ className }) {
+  const [visible, setVisible] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+
+  const onClick = () => {
+    setModalTitle("Faça sua Reserva");
+    setModalContent(
+      <p>
+        <div className="flex flex-col bg-white w-auto gap-2 py-2 justify-evenly m-auto">
+            {/* texto1 */}
+            {/* <div className="flex flex-col w-full items-center">
+              <div className="flex flex-col text-center w-auto">
+                <h1 className="font-semibold">FAÇA SUA RESERVA</h1>
+                <p className="text-paragraph2">Parcele em até 6x sem juros</p>
+              </div>
+            </div> */}
+            {/* check */}
+            <div className="w-full flex flex-col gap-4">
+              <div className="flex flex-col m-auto">
+                <p>Selecione</p>
+                <Dropdown
+                  value={selectedCity}
+                  onChange={(e) => setSelectedCity(e.value)}
+                  options={cities}
+                  optionLabel="name"
+                  className="w-[240px] phone2:w-[258px] h-[40px] font-mainFont bg-secondary/30 items-center"
+                />
+              </div>
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                {/* Flex-row para alinhar os DatePickers lado a lado */}
+                <div className="flex flex-col tablet1:flex-row gap-4 m-auto items-center">
+                  <div className="flex flex-col items-start mb-1">
+                    <p>Data de Entrada</p>
+                    <DatePicker
+                      value={checkInValue}
+                      onChange={(newValue) => setCheckInValue(newValue)}
+                      format="DD/MM/YYYY"
+                      sx={{
+                        width: "w-[80%] tablet1:w-auto",
+                        "& .MuiInputBase-root": {
+                          height: "40px",
+                          color: "#12759C",
+                        },
+                        "& .MuiSvgIcon-root": {
+                          color: "#12759C",
+                        },
+                        "& .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start mb-1">
+                    <p>Data de Saída</p>
+                    <DatePicker
+                      value={checkOutValue}
+                      onChange={(newValue) => setCheckOutValue(newValue)}
+                      format="DD/MM/YYYY"
+                      sx={{
+                        width: "w-[80%] tablet1:w-[30%]",
+                        "& .MuiInputBase-root": {
+                          height: "40px",
+                          color: "#12759C",
+                        },
+                        "& .MuiSvgIcon-root": {
+                          color: "#12759C",
+                        },
+                        "& .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#12759C",
+                        },
+                      }}
+                    />
+                  </div>
+                </div>
+              </LocalizationProvider>
+
+              <Button
+                onClick={handleClick}
+                className="mb-1 w-full phone3w-[78%] tablet1:w-[50%] h-[60px] m-auto"
+                gap={false}
+                size="small"
+                label="Reservar"
+              />
+            </div>
+          </div>
+      </p>
+    );
+    setVisible(true);
+  };
 
   const handleClick = () => {
     // Coletando os dados selecionados
-    const city = selectedCity?.name || "Não selecionado";  // Cidade
-    const checkInDate = dayjs(checkInValue?.$d).format("DD/MM/YYYY") || "Data não selecionada";  // Data de entrada
-    const checkOutDate = dayjs(checkOutValue?.$d).format("DD/MM/YYYY") || "Data não selecionada";  // Data de saída
-  
+    const city = selectedCity?.name || "Não selecionado"; // Cidade
+    const checkInDate =
+      dayjs(checkInValue?.$d).format("DD/MM/YYYY") || "Data não selecionada"; // Data de entrada
+    const checkOutDate =
+      dayjs(checkOutValue?.$d).format("DD/MM/YYYY") || "Data não selecionada"; // Data de saída
+
     // Montando a mensagem para o WhatsApp
     const message = `Olá! Gostaria de fazer uma reserva com os seguintes detalhes:
   - Cidade: ${city}
   - Data de Entrada: ${checkInDate}
   - Data de Saída: ${checkOutDate}`;
-  
+
     // Link do WhatsApp com a mensagem
-    const whatsappLink = `https://wa.me/21972613067?text=${encodeURIComponent(message)}`;
-  
+    const whatsappLink = `https://wa.me/21972613067?text=${encodeURIComponent(
+      message
+    )}`;
+
     // Abrir o link do WhatsApp
     window.open(whatsappLink, "_blank");
   };
@@ -33,9 +140,9 @@ export default function Reservation({ className }) {
   const [checkInValue, setCheckInValue] = useState(dayjs("2024-04-17"));
   const [checkOutValue, setCheckOutValue] = useState(dayjs("2024-04-18"));
 
-  const ButtonFloating = () => {
-    console.log("Ooi, sou o Botão das telas MENORES e estou funcionando Biell");
-  };
+  // const ButtonFloating = () => {
+  //   console.log("Ooi, sou o Botão das telas MENORES e estou funcionando Biell");
+  // };
 
   // Opções do Dropdown
   const cities = [
@@ -55,7 +162,7 @@ export default function Reservation({ className }) {
         >
           <div className="desktop1:hidden">
             <Button
-              onClick={ButtonFloating}
+              onClick={onClick}
               className="flex m-auto"
               label="fazer Reserva"
               size="small"
@@ -152,6 +259,20 @@ export default function Reservation({ className }) {
               />
             </div>
           </div>
+          <Dialog
+            className="font-secondFont"
+            header={modalTitle}
+            visible={visible}
+            onHide={() => setVisible(false)}
+            style={{ width: "50vw" }}
+            breakpoints={{
+              "4000px": "35vw",
+              "1024px": "60vw",
+              "641px": "90vw",
+            }}
+          >
+            <p className="m-0 ">{modalContent}</p>
+          </Dialog>
         </div>
       </SectionWrapper>
     </SectionArea>
