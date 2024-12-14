b4d246062c3735caf413d6acf9cc293a48115c06
Gabriel <gabrielsouza65115@gmail.com>
2024-12-14 09:48:26 -0300

    fix: Ajusta responsividade do modal em telas de tablet

--- src/components/sectionElements/Reservation.jsx
+++ src/components/sectionElements/Reservation.jsx
@@ -20,95 +20,107 @@ export default function Reservation({ className }) {
     setModalContent(
       <p>
         <div className="flex flex-col bg-white w-auto gap-2 py-2 justify-evenly m-auto">
-            {/* texto1 */}
-            {/* <div className="flex flex-col w-full items-center">
+          {/* texto1 */}
+          {/* <div className="flex flex-col w-full items-center">
               <div className="flex flex-col text-center w-auto">
                 <h1 className="font-semibold">FAÇA SUA RESERVA</h1>
                 <p className="text-paragraph2">Parcele em até 6x sem juros</p>
               </div>
             </div> */}
-            {/* check */}
-            <div className="w-full flex flex-col gap-4">
-              <div className="flex flex-col m-auto">
-                <p>Selecione</p>
-                <Dropdown
-                  value={selectedCity}
-                  onChange={(e) => setSelectedCity(e.value)}
-                  options={cities}
-                  optionLabel="name"
-                  className="w-[240px] phone2:w-[258px] h-[40px] font-mainFont bg-secondary/30 items-center"
-                />
-              </div>
-
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                {/* Flex-row para alinhar os DatePickers lado a lado */}
-                <div className="flex flex-col tablet1:flex-row gap-4 m-auto items-center">
-                  <div className="flex flex-col items-start mb-1">
-                    <p>Data de Entrada</p>
-                    <DatePicker
-                      value={checkInValue}
-                      onChange={(newValue) => setCheckInValue(newValue)}
-                      format="DD/MM/YYYY"
-                      sx={{
-                        width: "w-[80%] tablet1:w-auto",
-                        "& .MuiInputBase-root": {
-                          height: "40px",
-                          color: "#12759C",
-                        },
-                        "& .MuiSvgIcon-root": {
-                          color: "#12759C",
-                        },
-                        "& .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                        "&:hover .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                      }}
-                    />
-                  </div>
-                  <div className="flex flex-col items-start mb-1">
-                    <p>Data de Saída</p>
-                    <DatePicker
-                      value={checkOutValue}
-                      onChange={(newValue) => setCheckOutValue(newValue)}
-                      format="DD/MM/YYYY"
-                      sx={{
-                        width: "w-[80%] tablet1:w-[30%]",
-                        "& .MuiInputBase-root": {
-                          height: "40px",
-                          color: "#12759C",
-                        },
-                        "& .MuiSvgIcon-root": {
-                          color: "#12759C",
-                        },
-                        "& .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                        "&:hover .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "#12759C",
-                        },
-                      }}
-                    />
-                  </div>
-                </div>
-              </LocalizationProvider>
-
-              <Button
-                onClick={handleClick}
-                className="mb-1 w-full phone3w-[78%] tablet1:w-[50%] h-[60px] m-auto"
-                gap={false}
-                size="small"
-                label="Reservar"
+          {/* check */}
+          <div className="w-full flex flex-col gap-4">
+            <div className="flex flex-col m-auto">
+              <p>Selecione</p>
+              <Dropdown
+                value={selectedCity}
+                onChange={(e) => setSelectedCity(e.value)}
+                options={cities}
+                optionLabel="name"
+                className="w-[240px] phone2:w-[258px] h-[40px] font-mainFont bg-secondary/30 items-center"
               />
             </div>
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              {/* Flex-row para alinhar os DatePickers lado a lado */}
+              <div className="flex flex-col gap-4 m-auto items-center">
+                <div className="flex flex-col items-start mb-1">
+                  <p>Data de Entrada</p>
+                  <DatePicker
+                    value={checkInValue}
+                    onChange={(newValue) => setCheckInValue(newValue)}
+                    format="DD/MM/YYYY"
+                    sx={{
+                      width: "240px",
+                      "& .MuiInputBase-root": {
+                        height: "40px",
+                        color: "#12759C",
+                      },
+                       // responsividade
+                       "@media (min-width: 374px) and (max-width: 1024px)": {
+                        width: "258px",
+                        
+                      },
+                      "& .MuiSvgIcon-root": {
+                        color: "#12759C",
+                      },
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                      "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col items-start mb-1">
+                  <p>Data de Saída</p>
+                  <DatePicker
+                    value={checkOutValue}
+                    onChange={(newValue) => setCheckOutValue(newValue)}
+                    format="DD/MM/YYYY"
+                    sx={{
+                      width: "240px",
+                      "& .MuiInputBase-root": {
+                        height: "40px",
+                        color: "#12759C",
+                      },
+                      // responsividade
+                      "@media (min-width: 374px) and (max-width: 1024px)": {
+                        width: "258px",
+                        "& .MuiInputBase-root": {
+                        },
+                      },
+
+                      "& .MuiSvgIcon-root": {
+                        color: "#12759C",
+                      },
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                      "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#12759C",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            </LocalizationProvider>
+
+            <Button
+              onClick={handleClick}
+              className="mb-1 w-[240px] phone2:w-[258px] h-[40px] m-auto"
+              gap={false}
+              size="small"
+              label="Reservar"
+            />
           </div>
+        </div>
       </p>
     );
     setVisible(true);
@@ -266,9 +278,10 @@ export default function Reservation({ className }) {
             onHide={() => setVisible(false)}
             style={{ width: "50vw" }}
             breakpoints={{
-              "4000px": "35vw",
-              "1024px": "60vw",
-              "641px": "90vw",
+              "1024px": "300px",
+              "768px": "300px",
+              "639px": "300px",
+              "375px": "288px",
             }}
           >
             <p className="m-0 ">{modalContent}</p>
