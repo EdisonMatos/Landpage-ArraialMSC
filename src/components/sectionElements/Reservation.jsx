98244ec17c47a412aac9a4c713d9c4dd2077a3f5
Gabriel <gabrielsouza65115@gmail.com>
2024-12-16 09:47:27 -0300

    fix: Adiciona sombra ao container do componente

--- src/components/sectionElements/Reservation.jsx
+++ src/components/sectionElements/Reservation.jsx
@@ -181,7 +181,7 @@ export default function Reservation({ className }) {
             />
           </div>
 
-          <div className="hidden desktop1:flex bg-white w-[826px] rounded-sm gap-2 py-2 justify-evenly m-auto">
+          <div className="hidden desktop1:flex bg-white w-[826px] rounded-sm gap-2 py-2 justify-evenly m-auto shadow-md">
             {/* texto1 */}
             <div className="flex items-center desktop1:text-paragraph3">
               <div className="flex flex-col text-center w-auto">
